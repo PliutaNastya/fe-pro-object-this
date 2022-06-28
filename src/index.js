59d8a4cbc6ef2b423085f2325a29729386d5fd7a
Anastasiia <koicheva1996@gmail.com>
2022-06-28 14:47:56 +0300

    added homework (fe-pro-object-this)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+  getActualFreePlace: function () {
+    return this.quantityOfPlaces - this.getLength();
+  },
+  paidPerPlace: function () {
+    return (this.bankAccount = this.getLength() * this.priceByPlace);
+  },
+  checkInGuest: function (firstName, lastName, money) {
+    if (Object.keys(this.guests).length >= this.quantityOfPlaces) {
+      return 'Sorry, we have not free spaces';
+    } else if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    }
+    return (this.guests[this.getLength()] = {
+      firstName,
+      lastName,
+      money: money - this.priceByPlace,
+    });
+  },
+};
