b903964bece56c2880bf774dd7dae0c6b5edea6a
Anastasiia <koicheva1996@gmail.com>
2022-06-29 21:17:26 +0300

    correct hw

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ export const hotel = {
   priceByPlace: 20,
   bankAccount: 0,
   guests: {},
-  getLength: function () {
+  getLength() {
     return Object.keys(this.guests).length;
   },
-  getActualFreePlace: function () {
+  getActualFreePlace() {
     return this.quantityOfPlaces - this.getLength();
   },
-  paidPerPlace: function () {
-    this.bankAccount = this.getLength() * this.priceByPlace;
+  paidPerPlace() {
+    return (this.bankAccount = this.getLength() * this.priceByPlace);
   },
-  checkInGuest: function (firstName, lastName, money) {
+  checkInGuest(firstName, lastName, money) {
     if (this.getActualFreePlace() <= 0) {
       return 'Sorry, we have not free spaces';
     }
     if (money < this.priceByPlace) {
       return 'Sorry, you have not enough money';
     }
-    this.guests[this.getLength()] = {
+    return (this.guests[this.getLength()] = {
       firstName,
       lastName,
       money: money - this.priceByPlace,
-    };
+    });
     this.paidPerPlace();
   },
 };
